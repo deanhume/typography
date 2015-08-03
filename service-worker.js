84bebe970685f58d65b82277f9dff16ecf610984
Dean <deanhume@gmail.com>
2015-08-03 10:29:49 +0100

    Updated icon links

--- service-worker.js
+++ service-worker.js
@@ -23,7 +23,7 @@ self.addEventListener('push', function(event) {
 
   var title = 'Notification';  
   var body = 'There is newly updated content available on the site. Click to see more.';  
-  var icon = 'icons/typography.png';  
+  var icon = 'https://raw.githubusercontent.com/deanhume/typography/gh-pages/icons/typography.png';  
   var tag = 'simple-push-demo-notification-tag';
   
   event.waitUntil(  
