b5824f22bc67b105bf8ae5225d856feeea4994aa
Dean <deanhume@gmail.com>
2016-03-16 14:17:13 +0000

    Updated links

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,9 @@
-if ('serviceWorker' in navigator) { 
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-	// Registration was successful 
+	// Registration was successful
     console.log('ServiceWorker registration successful with scope: ',    registration.scope);
 	registration.pushManager.subscribe().then(function(subscription){
-		
-        isPushEnabled = true;  
-		
+    isPushEnabled = true;
 		console.log("subscription.subscriptionId: ", subscription.subscriptionId);
 		console.log("subscription.endpoint: ", subscription.endpoint);
 
@@ -19,53 +17,52 @@ if ('serviceWorker' in navigator) {
 		}).catch(function(err) {
 			console.log('There was an issue saving the subscription details: ', err);
 		});
-		
+
 	});
-	
+
   }).catch(function(err) {
     // registration failed :(
     console.log('ServiceWorker registration failed: ', err);
   });
 }
 
-self.addEventListener('push', function(event) {  
-	debugger;
+self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
 
-  var title = 'Notification';  
-  var body = 'There is newly updated content available on the site. Click to see more.';  
-  var icon = 'https://raw.githubusercontent.com/deanhume/typography/gh-pages/icons/typography.png';  
+  var title = 'Notification';
+  var body = 'There is newly updated content available on the site. Click to see more.';
+  var icon = 'https://raw.githubusercontent.com/deanhume/typography/gh-pages/icons/typography.png';
   var tag = 'simple-push-demo-notification-tag';
-  
-  event.waitUntil(  
-    self.registration.showNotification(title, {  
-       body: body,  
-       icon: icon,  
-       tag: tag  
-     })  
-   );  
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+       body: body,
+       icon: icon,
+       tag: tag
+     })
+   );
 });
 
-self.addEventListener('notificationclick', function(event) {  
-  console.log('On notification click: ', event.notification.tag);  
-  // Android doesn't close the notification when you click on it  
-  // See: http://crbug.com/463146  
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn't close the notification when you click on it
+  // See: http://crbug.com/463146
   event.notification.close();
 
-  // This looks to see if the current is already open and  
-  // focuses if it is  
+  // This looks to see if the current is already open and
+  // focuses if it is
   event.waitUntil(
-    clients.matchAll({  
-      type: "window"  
+    clients.matchAll({
+      type: "window"
     })
-    .then(function(clientList) {  
-      for (var i = 0; i < clientList.length; i++) {  
-        var client = clientList[i];  
-        if (client.url == '/' && 'focus' in client)  
-          return client.focus();  
-      }  
+    .then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url == '/' && 'focus' in client)
+          return client.focus();
+      }
       if (clients.openWindow) {
-        return clients.openWindow('https://deanhume.github.io/typography');  
+        return clients.openWindow('https://deanhume.github.io/typography');
       }
     })
   );
