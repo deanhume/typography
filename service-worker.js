0f84bd9a94ed3bf618229250dd6000f736ae49d7
Dean <deanhume@gmail.com>
2016-03-16 14:33:38 +0000

    Updated push service worker

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js').then(function(registration) {
 	// Registration was successful
     console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-	registration.pushManager.subscribe().then(function(subscription){
+	registration.pushManager.subscribe({userVisibleOnly: true}).then(function(subscription){
     isPushEnabled = true;
 		console.log("subscription.subscriptionId: ", subscription.subscriptionId);
 		console.log("subscription.endpoint: ", subscription.endpoint);
